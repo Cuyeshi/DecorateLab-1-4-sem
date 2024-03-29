075fa790905329c0bce6f02255b8bc7d8db2d9ec
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-03-29 14:41:26 +0300

    Добавьте файлы проекта.

---
+++ DecorateLab-1(4 sem)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DecorateLab_1_4_sem_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
